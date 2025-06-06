e5775d65f11a86703a5912c7c4e778b947f93e8f
garcia2202 <tonato.thiago@gmail.com>
2025-06-06 20:06:47 -0300

    .

--- src/main/java/com/firewatch/api/entity/Cidade.java
+++ src/main/java/com/firewatch/api/entity/Cidade.java
@@ -5,16 +5,17 @@ import lombok.*;
 
 @Data
 @Entity
-@Table(name = "cidade")
+@Table(name = "CIDADE")
 public class Cidade {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cidade_seq")
     @SequenceGenerator(name = "cidade_seq", sequenceName = "SEQ_CIDADE", allocationSize = 1)
-    @Column(name = "id_cidade")
+    @Column(name = "ID_CIDADE")
     private Long id;
 
-    @Column(name = "nome_cidade")
+    @Column(name = "NOME_CIDADE", nullable = false)
     private String nome;
 
+    @Column(name = "ESTADO")
     private String estado;
 }
