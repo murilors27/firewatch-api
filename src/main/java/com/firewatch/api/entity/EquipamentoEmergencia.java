773727b69fd5656b9a277910393a2b3a1abd9523
garcia2202 <tonato.thiago@gmail.com>
2025-06-06 20:34:27 -0300

    arrumado nome coluna

--- src/main/java/com/firewatch/api/entity/EquipamentoEmergencia.java
+++ src/main/java/com/firewatch/api/entity/EquipamentoEmergencia.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "equipamento_emergencia")
+@Table(name = "EQUIPAMENTO_EMERGENCIA")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,9 +13,9 @@ public class EquipamentoEmergencia {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "equipamento_seq")
     @SequenceGenerator(name = "equipamento_seq", sequenceName = "SEQ_EQUIPAMENTO", allocationSize = 1)
-    @Column(name = "id_equipamento_emergencia")
+    @Column(name = "ID_EQUIPAMENTO_EMERGENCIA")
     private Long id;
 
-    @Column(name = "nome_equipamento", nullable = false)
+    @Column(name = "NOME", nullable = false, length = 100)
     private String nome;
 }
