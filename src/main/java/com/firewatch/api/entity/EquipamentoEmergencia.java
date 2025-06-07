6a243778eff16d7abfe6d67fcce9ab304786c59d
garcia2202 <tonato.thiago@gmail.com>
2025-06-06 21:36:35 -0300

    kkkkkkkkkkk

--- src/main/java/com/firewatch/api/entity/EquipamentoEmergencia.java
+++ src/main/java/com/firewatch/api/entity/EquipamentoEmergencia.java
@@ -17,6 +17,6 @@ public class EquipamentoEmergencia {
     @Column(name = "ID_EQUIPAMENTO_EMERGENCIA")
     private Long id;
 
-    @Column(name = "NOME", nullable = false, length = 100)
+    @Column(name = "NOME_EQUIPAMENTO", nullable = false, length = 100)
     private String nome;
 }
