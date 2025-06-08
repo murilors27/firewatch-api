3c6403e91bcc7b86369c4bd23abcdb37d82b05a1
murilors27 <murilorsantos27@gmail.com>
2025-06-08 03:25:44 -0300

    fix: ajuste na url dos endpoints

--- src/main/java/com/firewatch/api/controller/EquipamentoController.java
+++ src/main/java/com/firewatch/api/controller/EquipamentoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/equipamentos")
+@RequestMapping("/api/equipamentos")
 public class EquipamentoController {
 
     private final EquipamentoService equipamentoService;
